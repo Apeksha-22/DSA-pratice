47bf69e16c9fdec1e318a4263497e75e641eee44
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-08-07 22:27:33 +0530

    Create First non-repeating character in a stream of characters.cpp

---
+++ First non-repeating character in a stream of characters.cpp
@@ -0,0 +1,19 @@
+string Solution::solve(string A) {
+    string B = "";
+    vector<int>temp(26,0);
+    queue<char>q;
+    
+    for(int i=0; i<A.size(); i++){
+        temp[A[i] - 'a'] +=1;
+        q.push(A[i]);
+        while(!q.empty() && temp[q.front() - 'a'] > 1){
+            q.pop();
+        }
+        if(temp[q.front() - 'a'] == 1) B += q.front();
+        else B += '#';
+    }
+    return B;
+}
+
+
+Question link: https://www.interviewbit.com/problems/first-non-repeating-character-in-a-stream-of-characters/
